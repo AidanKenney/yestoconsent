d266b56da7b0607028713bda399d64ffcec631ef
AidanKenney <kenneyaidan@gmail.com>
2020-12-03 16:34:38 -0500

    Insta HTML appended to DOM on render, no real pic
    - able to append HTML from Fetch request to DOM
    - however, HTML does not include actual photo we want to see

--- like_button.js
+++ like_button.js
@@ -8,8 +8,6 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-import axios from '../node_modules/axios/dist/axios.js';
-
 var LikeButton = function (_React$Component) {
   _inherits(LikeButton, _React$Component);
 
@@ -18,35 +16,27 @@ var LikeButton = function (_React$Component) {
 
     var _this = _possibleConstructorReturn(this, (LikeButton.__proto__ || Object.getPrototypeOf(LikeButton)).call(this, props));
 
-    _this.state = { liked: false };
+    _this.state = {
+      liked: false,
+      html: ''
+    };
     return _this;
   }
 
   _createClass(LikeButton, [{
     key: 'componentDidMount',
     value: function componentDidMount() {
-      axios({
-        url: "https://graph.facebook.com/v9.0/instagram_oembed?url=https://www.instagram.com/p/CH29f74rGeO/?utm_source=ig_web_copy_link/&access_token=395899871462736%7Cd2a5101e4de7d2f7a459f3ca204a7b03",
-        method: 'GET'
-        // headers: {
-        //   'Authorization': `Token token=${this.props.user.token}`
-        // }
-      }).then(function (res) {
-        return console.log(res);
+      var apiUrl = "https://graph.facebook.com/v9.0/instagram_oembed?url=https://www.instagram.com/p/CH29f74rGeO/?utm_source=ig_web_copy_link/&access_token=395899871462736%7Cd2a5101e4de7d2f7a459f3ca204a7b03";
+      fetch(apiUrl, {
+        method: 'get'
+      }).then(function (response) {
+        return response.json();
+      }).then(function (data) {
+        // this.setState({ html: data.html })
+        var insta = document.createElement('div');
+        insta.innerHTML = data.html;
+        document.getElementById("like_button_container").appendChild(insta);
       });
-      // .then(res => this.setState({ item: res.data.item }))
-      // .then(() => this.props.msgAlert({
-      //   heading: 'Success',
-      //   variant: 'success',
-      //   message: 'Success to view your item!'
-      // }))
-      // .catch(() => (
-      //   this.props.msgAlert({
-      //     heading: 'Failure',
-      //     variant: 'danger',
-      //     message: 'Error 404!!!!!!'
-      //   })
-      // ))
     }
   }, {
     key: 'render',
@@ -58,11 +48,15 @@ var LikeButton = function (_React$Component) {
       }
 
       return React.createElement(
-        'button',
-        { onClick: function onClick() {
-            return _this2.setState({ liked: true });
-          } },
-        'Like'
+        'div',
+        null,
+        React.createElement(
+          'button',
+          { onClick: function onClick() {
+              return _this2.setState({ liked: true });
+            } },
+          'Like'
+        )
       );
     }
   }]);
--- src/like_button.js
+++ src/like_button.js
@@ -1,35 +1,26 @@
 'use strict';
 
-import axios from '../node_modules/axios/dist/axios.js'
-
 class LikeButton extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { liked: false };
+    this.state = {
+      liked: false,
+      html: ''
+     };
   }
 
   componentDidMount () {
-    axios({
-      url: "https://graph.facebook.com/v9.0/instagram_oembed?url=https://www.instagram.com/p/CH29f74rGeO/?utm_source=ig_web_copy_link/&access_token=395899871462736%7Cd2a5101e4de7d2f7a459f3ca204a7b03",
-      method: 'GET'
-      // headers: {
-      //   'Authorization': `Token token=${this.props.user.token}`
-      // }
+    const apiUrl = "https://graph.facebook.com/v9.0/instagram_oembed?url=https://www.instagram.com/p/CH29f74rGeO/?utm_source=ig_web_copy_link/&access_token=395899871462736%7Cd2a5101e4de7d2f7a459f3ca204a7b03"
+    fetch(apiUrl, {
+    method: 'get'
     })
-      .then(res => console.log(res))
-      // .then(res => this.setState({ item: res.data.item }))
-      // .then(() => this.props.msgAlert({
-      //   heading: 'Success',
-      //   variant: 'success',
-      //   message: 'Success to view your item!'
-      // }))
-      // .catch(() => (
-      //   this.props.msgAlert({
-      //     heading: 'Failure',
-      //     variant: 'danger',
-      //     message: 'Error 404!!!!!!'
-      //   })
-      // ))
+    .then(response => response.json())
+    .then(data => {
+      // this.setState({ html: data.html })
+      const insta = document.createElement('div')
+      insta.innerHTML = data.html
+      document.getElementById("like_button_container").appendChild(insta)
+    })
   }
 
   render() {
@@ -38,9 +29,11 @@ class LikeButton extends React.Component {
     }
 
     return (
-      <button onClick={() => this.setState({ liked: true }) }>
-        Like
-      </button>
+      <div>
+        <button onClick={() => this.setState({ liked: true }) }>
+          Like
+        </button>
+      </div>
     );
   }
 }
